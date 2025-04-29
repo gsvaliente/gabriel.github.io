9b86490fec165e156669b082bdd84e0bd50e860d
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2025-04-29 15:01:26 +0200

    Refactored the Education content to be easier to add and modify

---
+++ src/consts/education.ts
@@ -0,0 +1,28 @@
+interface Props {
+  title: string
+  description: string
+  years: string
+}
+
+export const educationArray: Props[] = [
+  {
+    title: 'Bachelor of Science in Marketing',
+    description: 'I received my Bachelor from West Virginia University',
+    years: '2012-2016',
+  },
+  {
+    title: 'MBA in International Business',
+    description: 'Graduated from EU Business School',
+    years: '2018-2019',
+  },
+  {
+    title: 'BackEnd Development Bootcamp',
+    description: 'Received certification from Barcelona Activa',
+    years: '2022-2023',
+  },
+  {
+    title: 'FrontEnd Development Bootcamp',
+    description: 'Received certification from Barcelona Activa',
+    years: '2023-2024',
+  },
+]
