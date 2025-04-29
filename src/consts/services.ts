cf36f3c9ab9b26c3722801e50a040791cf34b929
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2025-04-29 14:46:23 +0200

    Refactored to modifying services cards easier

---
+++ src/consts/services.ts
@@ -0,0 +1,27 @@
+interface Service {
+  title: string
+  description: string
+}
+
+export const serviceArray: Service[] = [
+  {
+    title: 'Web Development',
+    description:
+      'I offer a range of web development services, from simple static sites to complex web applications.I offer a range of web development services, from simple static sites to complex web applications.',
+  },
+  {
+    title: 'UX/UI Design',
+    description:
+      'For me, it is essential to create a design that is not only functional but also aesthetically pleasing',
+  },
+  {
+    title: 'App Development',
+    description:
+      'Creating mobile and web applications using the latest technologies and trends',
+  },
+  {
+    title: 'CMS Support',
+    description:
+      'Content Management System (CMS) creating and support services, from serverless backend to Wordpress and Shopify',
+  },
+]
