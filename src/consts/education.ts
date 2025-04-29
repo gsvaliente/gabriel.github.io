e14f2df0a366456d229c4a7064a5d8e5f6fa9ec9
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2025-04-29 17:17:56 +0200

    Small order change in Education content side of the project

--- src/consts/education.ts
+++ src/consts/education.ts
@@ -6,14 +6,9 @@ interface Props {
 
 export const educationArray: Props[] = [
   {
-    title: 'Bachelor of Science in Marketing',
-    description: 'I received my Bachelor from West Virginia University',
-    years: '2012-2016',
-  },
-  {
-    title: 'MBA in International Business',
-    description: 'Graduated from EU Business School',
-    years: '2018-2019',
+    title: 'FrontEnd Development Bootcamp',
+    description: 'Received certification from Barcelona Activa',
+    years: '2023-2024',
   },
   {
     title: 'BackEnd Development Bootcamp',
@@ -21,8 +16,13 @@ export const educationArray: Props[] = [
     years: '2022-2023',
   },
   {
-    title: 'FrontEnd Development Bootcamp',
-    description: 'Received certification from Barcelona Activa',
-    years: '2023-2024',
+    title: 'MBA in International Business',
+    description: 'Graduated from EU Business School',
+    years: '2018-2019',
+  },
+  {
+    title: 'Bachelor of Science in Marketing',
+    description: 'I received my Bachelor from West Virginia University',
+    years: '2012-2016',
   },
 ]
