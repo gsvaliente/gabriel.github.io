95ecfce297fd15b0149ebc2a357c65c6efccca78
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2025-04-30 12:37:32 +0200

    Installed starlight package for Icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
+
+import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
-});
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [
+    starlight({
+      title: 'Gabriel Valiente',
+    }),
+  ],
+})
