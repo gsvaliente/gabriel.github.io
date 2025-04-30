4e80d46565fb2e55dac243484e4d64ce49ea1fe3
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2025-04-30 15:39:03 +0200

    Trying to publish on nelify 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gabriel.github.io',
-  base: '/gsvaliente/',
   vite: {
     plugins: [tailwindcss()],
   },
