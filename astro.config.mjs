693753529540cadfc0dec5e2e71ea4a4fe9beefa
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2025-04-30 13:52:21 +0200

    Trying to get to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://gabriel.github.io',
+  base: '/gsvaliente/',
   vite: {
     plugins: [tailwindcss()],
   },
